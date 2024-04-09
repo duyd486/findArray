bcec8dcc5a40ccdeb7dba066d5e7a7a06a971cbb
duyd486 <duyd486@gmail.com>
2024-04-09 19:52:11 +0700

    add du an
    add du an

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program {
+    public static void Main(string[] args)
+    {
+        string[] myStudents = { "Mai", "An", "Hao", "Hoang", "Tam", "Duy"};
+        Console.WriteLine("Nhap ten can tim: ");
+        string name = Console.ReadLine();
+
+        bool hasStudent = false;
+        for (int i = 0; i < myStudents.Length; i++) {
+            if (myStudents[i].Equals(name)) {
+                hasStudent = true;
+                Console.WriteLine("Sinh vien co mat");
+                break;
+            }
+        }
+        if (!hasStudent) {
+            Console.WriteLine("Khong co sinh vien");
+        }
+    }
+}
